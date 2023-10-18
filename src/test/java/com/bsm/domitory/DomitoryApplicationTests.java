6db25b95788968a76e85277c43a91e56382e8bc0
hyeonjoonpark <pjjoon1379@gmail.com>
2023-10-19 01:54:21 +0900

    FEAT :: 프로젝트 세팅

---
+++ src/test/java/com/bsm/domitory/DomitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsm.domitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomitoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
