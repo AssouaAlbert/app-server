f2a102c3139feedb0938fb10c2b680f3878d3667
hyeonjoonpark <pjjoon1379@gmail.com>
2023-10-19 10:18:12 +0900

    CORS 설정

--- src/main/java/com/bsm/domitory/DomitoryApplication.java
+++ src/main/java/com/bsm/domitory/DomitoryApplication.java
@@ -2,12 +2,25 @@ package com.bsm.domitory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DomitoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DomitoryApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOriginPatterns();
+			}
+		};
 	}
 
 }
